7f363fb50748e9cd43e0fc0a5dd43466571a6949
Pete Wall <pete@petewall.net>
2016-03-29 23:26:45 -0500

    Multithreading seems to work now.  It' runs on 8 threads and spins up the fans on my laptop!

--- src/main/java/edu/umn/kylepete/player/UCTPlayer.java
+++ src/main/java/edu/umn/kylepete/player/UCTPlayer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.ggp.base.apps.player.detail.DetailPanel;
 import org.ggp.base.apps.player.detail.SimpleDetailPanel;
@@ -20,6 +21,8 @@ import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
+import com.google.common.util.concurrent.AtomicDouble;
+
 public class UCTPlayer extends StateMachineGamer {
     @Override
     public String getName() {
@@ -31,8 +34,8 @@ public class UCTPlayer extends StateMachineGamer {
             this.parent = parent;
             this.state = state;
             this.children = new HashMap<List<Move>, StateNode>();
-            this.totalReward = 0;
-            this.visits = 0;
+            this.totalReward = new AtomicDouble(0);
+            this.visits = new AtomicInteger(0);
             this.depth = (parent != null ? parent.depth + 1 : 0);
         }
 
@@ -40,8 +43,8 @@ public class UCTPlayer extends StateMachineGamer {
         public StateNode parent;
         public MachineState state;
         public Map<List<Move>, StateNode> children;
-        public double totalReward;
-        public double visits;
+        public AtomicDouble totalReward;
+        public AtomicInteger visits;
 
         @Override
         public String toString() {
@@ -50,25 +53,58 @@ public class UCTPlayer extends StateMachineGamer {
     }
     private StateNode root = null;
     private int roleIndex;
+    private ArrayList<WorkerThread> threadPool;
+    long finishBy;
+
+    private class WorkerThread extends Thread {
+        public int iterations;
+
+        public WorkerThread(int id) {
+            super("Worker Thread " + id);
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (System.currentTimeMillis() < finishBy) {
+                    StateNode current = treePolicy(root);
+                    double value = defaultPolicy(current);
+                    backup(current, value);
+                    iterations++;
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
         long start = System.currentTimeMillis();
-        long finishBy = timeout - 1000;
+        finishBy = timeout - 1000;
 
         if (getLastMove() != null) {
             List<Move> theLastMove = getLastMove();
             root = root.children.get(theLastMove);
         }
 
-        int iterations = 0;
-        while (System.currentTimeMillis() < finishBy) {
-            StateNode current = treePolicy(root);
-            double value = defaultPolicy(current);
-            backup(current, value);
-            iterations++;
+        int totalIterations = 0;
+        for (WorkerThread thread : threadPool) {
+            thread.start();
         }
-        System.out.println("ran " + iterations + " iterations.");
+        for (int i = 0; i < threadPool.size(); ++i) {
+            try {
+                WorkerThread thread = threadPool.get(i);
+                thread.join();
+                totalIterations += thread.iterations;
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            threadPool.set(i, new WorkerThread(i));
+        }
+        System.out.println("ran " + totalIterations + " iterations.");
         long stop = System.currentTimeMillis();
         System.out.println("ran for " + (stop - start) / 1000.0 + " seconds");
 
@@ -87,20 +123,28 @@ public class UCTPlayer extends StateMachineGamer {
         return selection.get(roleIndex);
     }
 
-    private StateNode treePolicy(StateNode current) throws MoveDefinitionException, TransitionDefinitionException {
+    private StateNode treePolicy(StateNode source) throws MoveDefinitionException, TransitionDefinitionException {
+        StateNode current = source;
         StateMachine stateMachine = getStateMachine();
         while (!stateMachine.isTerminal(current.state)) {
             int validMoveCount = stateMachine.getLegalJointMoves(current.state).size();
             if (current.children.size() < validMoveCount) {
-                return expandNodes(current);
-            } else {
-                current = current.children.get(bestMoveSet(current, 1.44));
+                StateNode newNode = expandNodes(current);
+                if (newNode != null) {
+                    return newNode;
+                }
+                System.out.println("The new node was null!");
+            }
+            List<Move> moveset = bestMoveSet(current, 1.44);
+            current = current.children.get(moveset);
+            if (current == null) {
+                System.out.println("The best child was null!");
             }
         }
         return current;
     }
 
-    private StateNode expandNodes(StateNode source) throws MoveDefinitionException, TransitionDefinitionException {
+    private synchronized StateNode expandNodes(StateNode source) throws MoveDefinitionException, TransitionDefinitionException {
         StateMachine stateMachine = getStateMachine();
         List<List<Move>> allValidMoves = stateMachine.getLegalJointMoves(source.state);
         for (List<Move> moveset : allValidMoves) {
@@ -113,18 +157,32 @@ public class UCTPlayer extends StateMachineGamer {
             return node;
         }
 
+        System.out.println("Someone expanded them all!");
         return null;
     }
 
-    private List<Move> bestMoveSet(StateNode current, double explorationConstant) {
+    private synchronized List<Move> bestMoveSet(StateNode current, double explorationConstant) {
+        // FIXME: Should this be picking the opposite for the opponent's turn?
+        //        If that encourages deeper search on moves that the opponent is likely to make, it might be better.
+
         List<Move> bestMoveSet = null;
         double best = Double.NEGATIVE_INFINITY;
+        List<Double> values = new ArrayList<Double>();
 
         for (List<Move> moveset : current.children.keySet()) {
             StateNode child = current.children.get(moveset);
-            double UCB1 = (child.totalReward / child.visits);
-            if (explorationConstant != 0) {
-                UCB1 += explorationConstant * Math.sqrt((2.0 * Math.log(current.visits)) / child.visits);
+            double UCB1 = 0;
+            // if visits is 0, that means another thread has expanded the child nodes, but hasn't yet visited them
+            // FIXME: What's the appropriate response here?  For now, I'm skipping...
+            if (child.visits.intValue() > 0) {
+                UCB1 = (child.totalReward.doubleValue() / child.visits.intValue());
+                if (explorationConstant != 0) {
+                    UCB1 += explorationConstant * Math.sqrt((2.0 * Math.log(current.visits.doubleValue())) / child.visits.intValue());
+                }
+                values.add(new Double(UCB1));
+            }
+            if (Double.isNaN(UCB1)) {
+                System.out.println("Whuh oh!");
             }
             if (UCB1 > best) {
                 bestMoveSet = moveset;
@@ -132,20 +190,24 @@ public class UCTPlayer extends StateMachineGamer {
             }
         }
 
+        if (bestMoveSet == null) {
+            System.out.println("did i miss it?");
+        }
         return bestMoveSet;
     }
 
     private double defaultPolicy(StateNode current) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
-        int[] depth = new int[1];
-        MachineState terminalState = getStateMachine().performDepthCharge(current.state, depth);
+        System.out.println(Thread.currentThread().getName() + "DefaultPolicy: " + current);
+        MachineState startState = current.state;
+        MachineState terminalState = getStateMachine().performDepthCharge(startState, null);
         return getStateMachine().getGoal(terminalState, getRole()) / 100.0;
     }
 
     private void backup(StateNode node, double value) {
         StateNode current = node;
         do {
-            current.visits++;
-            current.totalReward += value;
+            current.visits.incrementAndGet();
+            current.totalReward.addAndGet(value);
             current = current.parent;
         } while (current != null);
     }
@@ -166,6 +228,13 @@ public class UCTPlayer extends StateMachineGamer {
         MachineState initialState = stateMachine.getInitialState();
         root = new StateNode(null, initialState);
         roleIndex = stateMachine.getRoles().indexOf(getRole());
+
+        int cores = Runtime.getRuntime().availableProcessors();
+//        cores = 2;
+        threadPool = new ArrayList<WorkerThread>(cores);
+        for (int i = 0; i < cores; ++i) {
+            threadPool.add(new WorkerThread(i));
+        }
     }
 
     @Override
