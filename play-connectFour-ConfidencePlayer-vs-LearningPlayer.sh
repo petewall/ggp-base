2330ebd72cad7bea87e07e1594b0d2e150b85268
Pete Wall <pete@petewall.net>
2016-05-01 21:43:23 -0500

    Adding a game of connect four with the confidence agent

---
+++ play-connectFour-ConfidencePlayer-vs-LearningPlayer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GAME_KEY=connectFour
+P1=ConfidenceAgent
+P2=LearningPlayer
+
+NUM_GAMES=1
+START_CLOCK=30
+PLAY_CLOCK=10
+
+./playTournament.sh $GAME_KEY $START_CLOCK $PLAY_CLOCK $NUM_GAMES $P1 $P2
+
